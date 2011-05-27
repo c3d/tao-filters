f52327dea5df3748bd80dfefffeaf25e5990757a
Soulisse Baptiste <soulisse@polytech.unice.fr>
2011-05-27 16:40:29 +0200

    Add Erosion and Black&White Filters to the filter module.

--- doc/filters.doxy.h
+++ doc/filters.doxy.h
@@ -14,7 +14,7 @@
  */
 
 /**
- * Apply a gaussian filter on current texture.
+ * Applies a Gaussian filter on current texture.
  * It can be used to add blur on an image for instance.
  *
  * @param iter maximum number of iteration of the filter.
@@ -22,7 +22,7 @@
 gaussian(iter:integer);
 
 /**
- * Apply a mean filter on the current texture.
+ * Applies a Mean filter on the current texture.
  * It can be used to remove noise of an image for instance.
  *
  * @param iter maximum number of iteration of the filter.
@@ -30,7 +30,7 @@ gaussian(iter:integer);
 mean(iter:integer);
 
 /**
- * Apply a embross filter on the current texture.
+ * Applies an Embross filter on the current texture.
  * It can be used to simulate 3D in an image for instance.
  *
  * @param iter maximum number of iteration of the filter.
@@ -38,7 +38,7 @@ mean(iter:integer);
 emboss(iter:integer);
 
 /**
- * Apply a sharpness filter on the current texture.
+ * Applies a Sharpness filter on the current texture.
  * It can be used to highlight details of an image for instance.
  *
  * @param iter maximum number of iteration of the filter.
@@ -46,12 +46,71 @@ emboss(iter:integer);
 sharpness(iter:integer);
 
 /**
- * Apply a laplacian filter on the current texture.
+ * Applies a Laplacian filter on the current texture.
  * It can be used to detect oulines of an image for instance.
  *
  * @param iter maximum number of iteration of the filter.
  */
 laplacian(iter:integer);
+
+/**
+ * Defines color levels for Black & White filter.
+ * These levels define the color tolerance into the Black & White filter.
+ *
+ * @param ratio_r ratio of the red component of the color. Its default values is 0.3.
+ * @param ratio_g ratio of the green component of the color. Its default values is 0.3.
+ * @param ratio_b ratio of the blue component of the color. Its default values is 0.3.
+ *
+ * @note This function is optionnal to apply Black & White filter
+ */
+black_and_white_levels(ratio_r:real, ratio_g:real, ratio_b:real);
+
+/**
+ * Applies a Black & White filter on the current texture.
+ * * It can be used to transform a color map into a black & white one.
+ *
+ * @note color levels can be defined for this filter thanks to @ref black_and_white_levels
+ *
+ */
+black_and_white();
+
+/**
+ * Defines erosion color.
+ * This color is used to determined regions to erode into the image.
+ *
+ * @param r red component of the color.
+ * @param g green component of the color.
+ * @param b blue component of the color.
+ * Its default value is the color of the erosion center.
+ *
+ * @note This function is optionnal to the Erosion filter.
+ */
+erode_color(r:real, g:real, b:real);
+
+/**
+ * Defines erosion radius.
+ * This radius defines the size of the erosion circle.
+ *
+ * @param radius radius of the erosion circle.
+ * Its default value is 1.0 (It corresponds to the entire image).
+ *
+ * @note This function is optionnal to the Erosion filter.
+ */
+erode_radius(radius:real);
+
+/**
+ * Applies an Erosion filter on the current texture.
+ * It can be used to erode some part of an image with a same color.
+ *
+ * @param x x-coordinate of the erosion center.
+ * @param y y-coordinate of the erosion center.
+ * @param threshold threshold of the erosion color.
+ *
+ * @note erosion color can be defined thanks to @ref erode_color.
+ * @note erosion radius can be defined thanks to @ref erode_radius.
+ *
+ */
+erode_radius(x:real, y:real, threshold:real);
 
 
 /**
