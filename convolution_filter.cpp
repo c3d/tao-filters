8eaec64bde7442a52c424945aed93e2d1cd42439
Jerome Forissier <jerome@taodyne.com>
2011-12-19 23:59:30 +0100

    Merge branch 'module_doc'

--- convolution_filter.cpp
+++ convolution_filter.cpp
@@ -46,6 +46,19 @@ ConvolutionFilter::ConvolutionFilter(uint unit, uint w, uint h)
 
         // Basic vertex shader
         static string vSrc =
+                "/********************************************************************************\n"
+                "**                                                                               \n"
+                "** Copyright (C) 2011 Taodyne.                                                   \n"
+                "** All rights reserved.                                                          \n"
+                "** Contact: Taodyne (contact@taodyne.com)                                        \n"
+                "**                                                                               \n"
+                "** This file is part of the Tao Presentations application, developped by Taodyne.\n"
+                "** It can be only used in the software and these modules.                        \n"
+                "**                                                                               \n"
+                "** If you have questions regarding the use of this file, please contact          \n"
+                "** Taodyne at contact@taodyne.com.                                               \n"
+                "**                                                                               \n"
+                "********************************************************************************/\n"
                 "varying vec4 color;"
                 "void main()"
                 "{"
@@ -61,6 +74,19 @@ ConvolutionFilter::ConvolutionFilter(uint unit, uint w, uint h)
                 "}";
 
         static string fSrc =
+                "/********************************************************************************\n"
+                "**                                                                               \n"
+                "** Copyright (C) 2011 Taodyne.                                                   \n"
+                "** All rights reserved.                                                          \n"
+                "** Contact: Taodyne (contact@taodyne.com)                                        \n"
+                "**                                                                               \n"
+                "** This file is part of the Tao Presentations application, developped by Taodyne.\n"
+                "** It can be only used in the software and these modules.                        \n"
+                "**                                                                               \n"
+                "** If you have questions regarding the use of this file, please contact          \n"
+                "** Taodyne at contact@taodyne.com.                                               \n"
+                "**                                                                               \n"
+                "********************************************************************************/\n"
                 "/* Filter parameters */"
                 "uniform int   width; "    //Texture width
                 "uniform int   height; "   //Texture height
