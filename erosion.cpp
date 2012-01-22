75b6f1a56e7f5258ce242893b378cd42a0db96cc
Jerome Forissier <jerome@taodyne.com>
2012-01-22 23:59:33 +0100

    Merge branch 'master' of ssh://nano.taodyne.com/~soulisse/Work/tao
    Conflicts:
    	modules/filters/black_and_white.cpp
    	modules/filters/convolution_filter.cpp
    	modules/filters/erosion.cpp
    	modules/shading/cel_shading.cpp
    	modules/shading/gooch_shading.cpp

--- erosion.cpp
+++ erosion.cpp
@@ -105,7 +105,7 @@ void Erosion::Draw()
         licensed = tao->checkLicense("Filters 1.0", false);
         tested = true;
     }
-    if (!licensed && !tao->blink(1.0, 0.2))
+    if (!licensed && !tao->blink(1.0, 0.2, 300.0))
         return;
 
     checkGLContext();
