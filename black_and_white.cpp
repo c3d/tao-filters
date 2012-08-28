fd3259e4a8dca4ce7d9a30b791739e09b3fd7434
Baptiste Soulisse <baptiste.soulisse@taodyne.com>
2012-08-28 09:23:10 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~jerome/Work/tao

--- black_and_white.cpp
+++ black_and_white.cpp
@@ -71,12 +71,9 @@ void BlackAndWhite::Draw()
 {
     if (!tested)
     {
-        licensed = tao->checkImpressOrLicense("Filters 1.0");
+        licensed = tao->checkImpressOrLicense("Filters 1.004");
         tested = true;
     }
-
-    if (!licensed && !tao->blink(1.0, 1.0, 300.0))
-        return;
 
     checkGLContext();
 
