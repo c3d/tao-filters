47d084a6da4ac8a8040d55c82901b29fa2ba0cd5
Christophe de Dinechin <christophe@taodyne.com>
2012-09-14 18:12:10 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~ddd/Work/tao

--- black_and_white.cpp
+++ black_and_white.cpp
@@ -71,7 +71,7 @@ void BlackAndWhite::Draw()
 {
     if (!tested)
     {
-        licensed = tao->checkImpressOrLicense("Filters 1.0");
+        licensed = tao->checkImpressOrLicense("Filters 1.004");
         tested = true;
     }
 
