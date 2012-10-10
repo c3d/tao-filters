c16d47274366d16dc6a8f74edb2f938f7823f5fe
Christophe de Dinechin <christophe@taodyne.com>
2012-10-10 17:19:52 +0200

    Add one level of indirection for saving/restoring state, refs #2556

--- black_and_white.cpp
+++ black_and_white.cpp
@@ -71,7 +71,7 @@ void BlackAndWhite::Draw()
 {
     if (!tested)
     {
-        licensed = tao->checkImpressOrLicense("Filters 1.005");
+        licensed = tao->checkImpressOrLicense("Filters 1.004");
         tested = true;
     }
 
